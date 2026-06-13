ef3dd9cf5d551c850898242a5936c524dc9cc576
Jeff Tian <jeff.tian@outlook.com>
2026-06-13 19:45:59 +0800

    chore(Formula/git-auto-commit, bin/git-auto-commit): bump version to 0.2.2
    Update the version number in both the formula and the script to reflect
    the new release. This ensures consistency across the project and
    prepares for the next deployment.
    
    Signed-off-by: Jeff Tian <jeff.tian@outlook.com>

--- Formula/git-auto-commit.rb
+++ Formula/git-auto-commit.rb
@@ -4,7 +4,7 @@ class GitAutoCommit < Formula
   url "https://github.com/Jeff-Tian/homebrew-tools.git",
       branch: "main",
       using:  :git
-  version "0.2.1"
+  version "0.2.2"
   license "MIT"
   head "https://github.com/Jeff-Tian/homebrew-tools.git", branch: "main"
 
